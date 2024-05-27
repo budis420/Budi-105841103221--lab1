50d6b362eac2afe5b3eaa05e8ab3ef67a1587d07
budis420 <105041103221@student.unismuh.ac.id>
2024-05-27 15:02:42 +0800

    Component

---
+++ Component/Button.js
@@ -0,0 +1,23 @@
+import { View } from 'react-native';
+
+const ButtonCostum = ({backgroundColor, text}) => {
+    return (
+        <View style={{
+            backgroundColor: backgroundColor,
+            width: 150,
+            height: 80,
+            borderRadius: 10,
+            marginRight: 10
+          }}>
+            <Text style={{
+                color: 'white',
+                textAlign: 'center',
+                lineHeight: 80,
+                fontSize: 30,
+                fontWeight: 'bold',
+              }}>
+                {text}
+            </Text>
+        </View>
+    );
+}
