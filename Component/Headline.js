ea780e1037e130b164e580fe82002277c4d3c04e
budis420 <105041103221@student.unismuh.ac.id>
2024-06-22 01:56:20 +0800

    react navigation

--- Component/Headline.js
+++ Component/Headline.js
@@ -1,18 +1,35 @@
+
 import React from 'react';
 import { View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useFonts } from 'expo-font';
 
-const Headline = ({text}) => {
+const Headline = ({text = "Default Text", color = "black", fontSize = 34}) => {
+    const [fontsLoaded] = useFonts({
+        'Metro-Bold' : require('../assets/fonts/Metropolis-Bold.otf'),
+        'Metro-Medium' : require('../assets/fonts/Metropolis-Medium.otf'),  
+        'SemiBold' : require('../assets/fonts/Metropolis-SemiBold.otf'),
+        'Black' : require('../assets/fonts/Metropolis-Black.otf'),
+    });
+
+    if (!fontsLoaded) {
+        return (
+            <View>
+                <Text>Font tidak di temukan!!!</Text>
+            </View>
+        );
+    }
+
     return (
        <SafeAreaView>
         <View style={{
             alignItems: 'Right',
-            marginLeft: 20,
-            marginTop: 20,
+            marginLeft: 20
         }}>
             <Text style={{
-                fontSize :  34,
-                fontWeight: 'bold'
+                fontSize :  Number(fontSize),
+                fontFamily: 'Metro-Bold',
+                color: color,
                 }}>
                 {text}
             </Text>
